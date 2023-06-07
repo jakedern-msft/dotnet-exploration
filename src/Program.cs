f88cd2dbde1c2bd6905ff53ab32434d89785a832
Jake Dern <jakedern@microsoft.com>
2023-06-07 21:50:15 +0000

    checkpoint

---
+++ src/Program.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.Extensions.Logging;
+using OpenTelemetry.Logs;
+
+using var loggerFactory = LoggerFactory.Create(builder =>
+{
+    builder.AddOpenTelemetry(options =>
+    {
+        options.AddConsoleExporter();
+        options.IncludeScopes = true;
+    });
+});
+
+var logger = loggerFactory.CreateLogger<Program>();
+using(logger.BeginScope(new List<KeyValuePair<string, object>>
+{
+    new KeyValuePair<string, object>("scope_name", "parent"),
+}))
+{
+    int id = 0;
+    while(true)
+    {
+        logger.LogInformation(eventId: id++, "Doing some structured things {foo}", "bar");
+        await Task.Delay(1000);
+    }
+}
